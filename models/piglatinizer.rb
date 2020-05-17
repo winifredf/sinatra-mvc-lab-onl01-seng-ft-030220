48306db25cc4a92d96d8abfe1402a52f0ef9c811
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 18:54:00 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,4 +3,23 @@ class Piglatinizer
     character.match(/[aeiiouAEIOU]/)
   end
   
+  def Piglatinize(word)
+    word.gsub(/[^a-z0-9\s]/i, '')
+    if word.split(" ").length == 1
+      piglatinize_word(word)
+    else
+      to_pig_latin(word)
+    end
+  end
+  
+  def piglatinize_word(word)
+    if vowel(word[0])
+      "#{word}way"
+    elsif !vowel(word[0]) && !vowel(word[1])
+      word = word.slice(3..-1) + word.slice(0, 3)
+      "#{word}ay"
+    elsif !vowel(word[0])
+      word = word.slice(1..-1) + word.slice(0)
+      "#{word}ay"
+    end
 end
